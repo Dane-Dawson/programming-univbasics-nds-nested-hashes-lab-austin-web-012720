a626c73cf5e678b726dff5b3846fdebb812f176c
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-15 02:51:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,7 @@ end
 
 def literal_aoh
   aoh = [
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+    DON_G  { name:  "Don Gately", occupation: "Live-in Staff" },
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
 KATE_G = { name:  "Kate Gompert", occupation: "None" },
